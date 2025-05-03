140b4a8193fc896839be7d6c98f0b24500680d55
Tung Duong Ta <duongtatung@gmail.com>
2025-05-03 17:47:45 +1000

    upgraade tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -61,10 +61,10 @@ export default function Home() {
           <div className="flex items-center justify-center "></div>
         </section>
         <Spotlight />
-        <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0 ">
+        <div className="mx-auto min-h-screen max-w-(--breakpoint-xl) px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0 ">
           <a
             href="#content"
-            className="absolute left-0 top-0 block -translate-x-full rounded bg-gradient-to-br from-teal-400 via-blue-500 to-purple-600 px-4 py-3 text-sm font-bold uppercase tracking-widest text-white focus-visible:translate-x-0"
+            className="absolute left-0 top-0 block -translate-x-full rounded bg-linear-to-br from-teal-400 via-blue-500 to-purple-600 px-4 py-3 text-sm font-bold uppercase tracking-widest text-white focus-visible:translate-x-0"
           >
             Skip to Content
           </a>
